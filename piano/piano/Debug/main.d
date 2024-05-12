9aa8bd935e7b0912a7a105f83526e2249883715a
Nipunikumudika <nipunikumudika@gmail.com>
2024-05-12 22:22:37 +0530

    interrupts

--- piano/piano/Debug/main.d
+++ piano/piano/Debug/main.d
@@ -13,6 +13,7 @@ main.d main.o: .././main.c \
  d:\programfiles\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  d:\programfiles\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  d:\programfiles\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ d:\programfiles\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
  .././LCD.h .././pin_config.h \
  d:\programfiles\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
  d:\programfiles\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
@@ -46,6 +47,8 @@ d:\programfiles\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\u
 d:\programfiles\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 d:\programfiles\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+d:\programfiles\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
 
 .././LCD.h:
 
--- piano/piano/main.c
+++ piano/piano/main.c
@@ -3,9 +3,15 @@
 #define F_CPU 16000000UL
 #define __DELAY_BACKWARD_COMPATIBLE__
 #include <util/delay.h>
+#include <avr/interrupt.h>
 #include "LCD.h"
 #include <stdio.h>
 #include <string.h>
+
+
+#define INTERRUPT_PIN_PD3 PD3
+#define INTERRUPT_PIN_PD5 PD5
+
 
 #define F3    2.8641
 #define Gb3   2.7027
@@ -43,8 +49,74 @@
 #define BUZZER_PORT     PORTB
 #define BUZZER_PIN      3
 
-void Play_Sound(float frequency);
-void stop_sound(void);
+void Play_Sound_violin(float frequency)
+{
+	float duration=5;
+	long int i,cycles;
+	float wavelength = 1.25;
+
+	cycles=duration/wavelength;
+
+	BUZZER_DDR |= (1 << BUZZER_PIN);
+
+
+		for(int j=0;j<20;j++){
+			_delay_ms(frequency / 10.5);
+			BUZZER_PORT |= (1 << BUZZER_PIN);
+			_delay_ms(22 * frequency / 10.5); // Ensuring the same duty cycle for both on and off times
+			BUZZER_PORT &= ~(1 << BUZZER_PIN);
+		}
+		for(int j=0;j<10;j++){
+			_delay_ms(frequency / 10.55);
+			BUZZER_PORT |= (1 << BUZZER_PIN);
+			_delay_ms(22.1 * frequency / 10.55); // Ensuring the same duty cycle for both on and off times
+			BUZZER_PORT &= ~(1 << BUZZER_PIN);
+		}
+		
+		for(int j=0;j<5;j++){
+			_delay_ms(frequency / 10.6);
+			BUZZER_PORT |= (1 << BUZZER_PIN);
+			_delay_ms(22.2 * frequency / 10.6); // Ensuring the same duty cycle for both on and off times
+			BUZZER_PORT &= ~(1 << BUZZER_PIN);
+		}
+		for(int j=0;j<10;j++){
+			_delay_ms(frequency / 10.55);
+			BUZZER_PORT |= (1 << BUZZER_PIN);
+			_delay_ms(22.1 * frequency / 10.55); // Ensuring the same duty cycle for both on and off times
+			BUZZER_PORT &= ~(1 << BUZZER_PIN);
+		}
+
+	
+}
+
+void Play_Sound_piano(float frequency)
+{
+
+	float duration=5;
+	long int i,cycles;
+	float wavelength = 1.25;
+	
+	cycles=duration/wavelength;
+
+	BUZZER_DDR |= (1 << BUZZER_PIN);
+
+	for (i=0;i<cycles;i++)
+	{
+		_delay_ms(frequency);
+		BUZZER_PORT |= (1 << BUZZER_PIN);
+		_delay_ms(frequency);
+		BUZZER_PORT &= ~(1 << BUZZER_PIN);
+	}
+	
+	
+}
+
+
+void stop_sound(void)
+{
+	// Stop sound
+	BUZZER_PORT &= ~(1 << BUZZER_PIN);
+}
 
 
 /*74HC165 read function*/
@@ -77,14 +149,14 @@ volatile uint8_t readInput2(void){
 	uint8_t reader = 0;
 
 	/* Set PIND3 (PD3) as input */
-	DDRD &= ~(1 << PD3);
+	DDRB &= ~(1 << PB4);
 
 	/* Raise LD pin */
 	PORTC |= (1 << PC1);
 
 	for (int i = 0; i < 8; i++){
 		reader <<= 1;   /* Get a one-bit digital input */
-		reader |= ((PIND & (1 << PD3)) ? 1 : 0);
+		reader |= ((PINB & (1 << PB4)) ? 1 : 0);
 		
 		/* Clock the register */
 		PORTC &= ~(1 << PC0);
@@ -98,18 +170,19 @@ volatile uint8_t readInput2(void){
 	return reader;
 }
 
+
 volatile uint8_t readInput3(void){
 	uint8_t reader = 0;
 
 	/* Set PIND2 (PD2) as input */
-	DDRD &= ~(1 << PD2);
+	DDRB &= ~(1 << PB5);
 
 	/* Raise LD pin */
 	PORTC |= (1 << PC1);
 
 	for (int i = 0; i < 8; i++){
 		reader <<= 1;   /* Get a one-bit digital input */
-		reader |= ((PIND & (1 << PD2)) ? 1 : 0);
+		reader |= ((PINB & (1 << PB5 )) ? 1 : 0);
 		
 		/* Clock the register */
 		PORTC &= ~(1 << PC0);
@@ -122,271 +195,167 @@ volatile uint8_t readInput3(void){
 
 	return reader;
 }
+
+
+volatile float calculateFrequency(void){
+	float freq = 0;
+/* Read the input value */
+uint8_t switchNo = readInput1();
+uint8_t switchNo2 = readInput2();
+uint8_t switchNo3 = readInput3();
+
+	/* Iterate through each bit of switchNo */
+	for (int i = 0; i < 8; i++) {
+		/* Check if the i-th bit of switchNo is set */
+		if (switchNo & (1 << i)) {
+			/* Set frequency based on which button is pressed */
+			switch (i) {
+				case 0:freq += F3;break;
+				case 1:freq += Gb3;break;
+				case 2:freq += G3;break;
+				case 3:freq += Ab3;break;
+				case 4:freq += A3;break;
+				case 5:freq += Bb3;break;
+				case 6:freq += B3;break;
+				case 7:freq += C4;break;
+				default:break;
+			}
+		}
+	}
+	
+	
+	
+	for (int i = 0; i < 8; i++) {
+		/* Check if the i-th bit of switchNo is set */
+		if (switchNo2 & (1 << i)) {
+			/* Set frequency based on which button is pressed */
+			switch (i) {
+				case 0:freq += Db4;break;
+				case 1:freq += D4;break;
+				case 2:freq += Eb4;break;
+				case 3:freq += E4;break;
+				case 4:freq += F4;break;
+				case 5:freq += Gb4;break;
+				case 6:freq += G4;break;
+				case 7:freq += Ab4;break;
+				default:break;
+			}
+		}
+	}
+	
+	
+	for (int i = 0; i < 8; i++) {
+		/* Check if the i-th bit of switchNo is set */
+		if (switchNo3 & (1 << i)) {
+			/* Set frequency based on which button is pressed */
+			switch (i) {
+				case 0:freq += A4;break;
+				case 1:freq += Bb4;break;
+				case 2:freq += B4;break;
+				case 3:freq += C5;break;
+				case 4:freq += Db5;break;
+				case 5:freq += D5;break;
+				case 6:freq += Eb5;break;
+				case 7:freq += E5;break;
+				default:
+				break;
+			}
+		}
+	}
+
+
+	return freq;
+}
+
+uint8_t instrument = -1;
+uint8_t bluetooth = -1;
+
+ISR( INT0_vect )
+{
+	if (PIND & (1 << PD2)) {
+		instrument=0;
+		lcd_goto_xy(1, 0);
+		lcd_write_word("Play Piano......");
+	}else{
+		instrument=1;
+		lcd_goto_xy(1, 0);
+		lcd_write_word("Play Violin.....");
+		
+	}
+	
+}
+
+ISR( INT1_vect )
+{
+	if (PIND & (1 << PD3)) {
+		lcd_goto_xy(0, 0);
+		lcd_write_word("Bluetooth.......");
+		bluetooth=1;
+	}else{
+		lcd_goto_xy(0, 0);
+		lcd_write_word("Toy Piano Music!");
+		bluetooth=0;
+	}
+}
+
 int main(void)
 {
 lcd_init();
 lcd_clear();
 _delay_ms (50);
-lcd_goto_xy(0, 0);
-lcd_write_word("Toy Piano Music!");
-	Play_Sound_new(F3);
 
-	Play_Sound_new(Gb3);
-
-	Play_Sound_new(G3);
-
-	Play_Sound_new(Ab3);
-
-	Play_Sound_new(A3);
-
-	Play_Sound_new(Bb3);
-
-	Play_Sound_new(B3);
-
-	Play_Sound_new(C4);
-	Play_Sound_new(Db4);
-
-	Play_Sound_new(D4);
-
-	Play_Sound_new(Eb4);
-
-	Play_Sound_new(E4);
-
-	Play_Sound_new(F4);
-
-	Play_Sound_new(Gb4);
-
-Play_Sound_new(G4);
-
-Play_Sound_new(Ab4);
-
-Play_Sound_new(A4);
-
-Play_Sound_new(Bb4);
-
-Play_Sound_new(B4);
-
-Play_Sound_new(C5);
-
-Play_Sound_new(Db5);
-
-Play_Sound_new(D5);
-
-Play_Sound_new(Eb5);
-
-Play_Sound_new(E5);
-
-
-_delay_ms(2000);
-
-
-Play_Sound(F3);
-
-Play_Sound(Gb3);
-
-Play_Sound(G3);
-
-Play_Sound(Ab3);
-
-Play_Sound(A3);
-
-Play_Sound(Bb3);
-
-Play_Sound(B3);
-
-Play_Sound(C4);
-Play_Sound(Db4);
-
-Play_Sound(D4);
-
-Play_Sound(Eb4);
-
-Play_Sound(E4);
-
-Play_Sound(F4);
-
-Play_Sound(Gb4);
-
-Play_Sound(G4);
-
-Play_Sound(Ab4);
-
-Play_Sound(A4);
-
-Play_Sound(Bb4);
-
-Play_Sound(B4);
-
-Play_Sound(C5);
-
-Play_Sound(Db5);
-
-Play_Sound(D5);
-
-Play_Sound(Eb5);
-
-Play_Sound(E5);
-
+	
 
 	/* Initialize PORTC and set PC0, PC1, PC2 as output */
 	DDRC |= 0b11111111;
 	DDRD |= 0b11111111;
-	while (1){
-		/* Read the input value */
-		uint8_t switchNo = readInput1();
-		uint8_t switchNo2 = readInput2();
-		uint8_t switchNo3 = readInput3();
-		
-		float freq = 0;
-		
-		if(PINB & 0B00000001){
-			/* Iterate through each bit of switchNo */
-			for (int i = 0; i < 8; i++) {
-				/* Check if the i-th bit of switchNo is set */
-				if (switchNo & (1 << i)) {
-					/* Set frequency based on which button is pressed */
-					switch (i) {
-						case 0:freq += F3;break;
-						case 1:freq += Gb3;break;
-						case 2:freq += G3;break;
-						case 3:freq += Ab3;break;
-						case 4:freq += A3;break;
-						case 5:freq += Bb3;break;
-						case 6:freq += B3;break;
-						case 7:freq += C4;break;
-						default:break;
-					}
-				}
-			}
-			
-			
-			
-			for (int i = 0; i < 8; i++) {
-				/* Check if the i-th bit of switchNo is set */
-				if (switchNo2 & (1 << i)) {
-					/* Set frequency based on which button is pressed */
-					switch (i) {
-						case 0:freq += Db4;break;
-						case 1:freq += D4;break;
-						case 2:freq += Eb4;break;
-						case 3:freq += E4;break;
-						case 4:freq += F4;break;
-						case 5:freq += Gb4;break;
-						case 6:freq += G4;break;
-						case 7:freq += Ab4;break;
-						default:break;
-					}
-				}
-			}
-			
-			
-			for (int i = 0; i < 8; i++) {
-				/* Check if the i-th bit of switchNo is set */
-				if (switchNo3 & (1 << i)) {
-					/* Set frequency based on which button is pressed */
-					switch (i) {
-						case 0:freq += A4;break;
-						case 1:freq += Bb4;break;
-						case 2:freq += B4;break;
-						case 3:freq += C5;break;
-						case 4:freq += Db5;break;
-						case 5:freq += D5;break;
-						case 6:freq += Eb5;break;
-						case 7:freq += E5;break;
-						default:
-						break;
-					}
-				}
-			}
-			}
-		
-		
-		if (freq > 0) {
-			Play_Sound(freq);
-			}else{
-			stop_sound();
-		}
-		
-
-	}
-}
-
-#include <math.h>
-
-#define PI 3.14159265
-
-#define SAMPLE_RATE     30000  // Adjust as needed
-
-
-void Play_Sound_new(float frequency)
-{
-	float duration=5;
-	long int i,cycles;
-	float wavelength = 1.25;
-
-	cycles=duration/wavelength;
-
-	BUZZER_DDR |= (1 << BUZZER_PIN);
-
-
-	for (i = 0; i < 10; i++)
-	{
-
-		for(int j=0;j<20;j++){
-			_delay_ms(frequency / 10.5);
-			BUZZER_PORT |= (1 << BUZZER_PIN);
-			_delay_ms(22 * frequency / 10.5); // Ensuring the same duty cycle for both on and off times
-			BUZZER_PORT &= ~(1 << BUZZER_PIN);
-		}
-		for(int j=0;j<10;j++){
-			_delay_ms(frequency / 10.55);
-			BUZZER_PORT |= (1 << BUZZER_PIN);
-			_delay_ms(22.1 * frequency / 10.55); // Ensuring the same duty cycle for both on and off times
-			BUZZER_PORT &= ~(1 << BUZZER_PIN);
-		}
-		
-		for(int j=0;j<5;j++){
-			_delay_ms(frequency / 10.6);
-			BUZZER_PORT |= (1 << BUZZER_PIN);
-			_delay_ms(22.2 * frequency / 10.6); // Ensuring the same duty cycle for both on and off times
-			BUZZER_PORT &= ~(1 << BUZZER_PIN);
-		}
-		for(int j=0;j<10;j++){
-			_delay_ms(frequency / 10.55);
-			BUZZER_PORT |= (1 << BUZZER_PIN);
-			_delay_ms(22.1 * frequency / 10.55); // Ensuring the same duty cycle for both on and off times
-			BUZZER_PORT &= ~(1 << BUZZER_PIN);
-		}
-
-	}
-
-}
-
-void Play_Sound(float frequency)
-{
-	for(int i=0;i<200;i++){
-		float duration=5;
-		long int i,cycles;
-		float wavelength = 1.25;
-		
-		cycles=duration/wavelength;
-
-		BUZZER_DDR |= (1 << BUZZER_PIN);
-
-		for (i=0;i<cycles;i++)
-		{
-			_delay_ms(frequency);
-			BUZZER_PORT |= (1 << BUZZER_PIN);
-			_delay_ms(frequency);
-			BUZZER_PORT &= ~(1 << BUZZER_PIN);
-		}
-	}
 	
-}
+	  EICRA |= (1 << ISC00);    // set INT0 to trigger on ANY logic change
+	  EIMSK |= (1 << INT0);     // Turns on INT0
+	
+	EICRA |= (1 << ISC10);    // set INT1 to trigger on ANY logic change
+	EIMSK |= (1 << INT1);     // Turns on INT1
+	
+	if (PIND & (1 << PD2)) {
+		instrument=0;
+		lcd_goto_xy(1, 0);
+		lcd_write_word("Play Piano......");
+		}else{
+		instrument=1;
+		lcd_goto_xy(1, 0);
+		lcd_write_word("Play Violin.....");
+		
+	}
+	if (PIND & (1 << PD3)) {
+		lcd_goto_xy(0, 0);
+		lcd_write_word("Bluetooth.......");
+		bluetooth=1;
+	}else{
+		lcd_goto_xy(0, 0);
+		lcd_write_word("Toy Piano Music!");
+		bluetooth=0;
+	}
+	sei();			/* Enable Global Interrupt */
+	
+	
+	
+	while (1){
+		float frequecy = calculateFrequency();
+		
+			if (frequecy > 0) {
+				if(instrument==0 && bluetooth==0){
+					Play_Sound_piano(frequecy);
+				}else if(instrument==1 && bluetooth==0){
+					Play_Sound_violin(frequecy);
+				}
+				
+				}else{
+				stop_sound();
+			}
+	
+			
+		
+		
 
-
-void stop_sound(void)
-{
-	// Stop sound
-	BUZZER_PORT &= ~(1 << BUZZER_PIN);
+	}
 }
